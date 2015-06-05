f8a1779e04be5f22d5b97c18dc31b4752d49e4e8
GitOutATown <aqumegeemail@gmail.com>
2015-06-05 12:31:13 -0700

    backup

--- src/main/scala/oreilly/learningspark/ch2/IntroLab1.scala
+++ src/main/scala/oreilly/learningspark/ch2/IntroLab1.scala
@@ -8,21 +8,21 @@ object IntroLab1 {
     def main (args: Array[String]): Unit = {
         val sc = new SparkContext("local", "IntroLab1", System.getenv("SPARK_HOME"))
         
-        val textFile = sc.textFile("resources/README_sample_text.md")
+        val textFile = sc.textFile("resources/README_sample_text.md") // RDD[String]
         
-        val textCnt = textFile.count() // Action
+        val textCnt = textFile.count() // Action -> Long
         println(s"textCnt: $textCnt")
         
-        val textFirst = textFile.first()
+        val textFirst = textFile.first() // Action -> String
         
-        val linesWithSpark = textFile.filter { line => line.contains("Spark") }
+        val linesWithSpark = textFile.filter { line => line.contains("Spark") } // RDD[String]
         println(s"linesWithSpark: " + linesWithSpark)
         println(s"linesWithSpark.count: " + linesWithSpark.count)
-        val lwsCache = linesWithSpark.cache()
+        val lwsCache = linesWithSpark.cache() // RDD[String]
         println(s"lwsCache: $lwsCache")
         
-        val longestLine = textFile.map(line => 
-            line.split(" ").size).reduce((a,b) => Math.max(a, b))
+        val lengthOfLongestLine = textFile.map(line => 
+            line.split(" ").size).reduce((a,b) => Math.max(a, b)) // Int
             
         val wordsCount1 = textFile.flatMap(line => 
             line.split(" ")).map(word => 
@@ -42,6 +42,6 @@ object IntroLab1 {
         println("=========>> wordsCount2.collect:")
         wordsCount2.collect.foreach(println(_))
         */
-        
+        wordsCount1.saveAsTextFile("resources/README_sample_counts.md")
     }
 }
