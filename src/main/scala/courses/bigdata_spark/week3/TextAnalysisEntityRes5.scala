2179c026bb4ebff72144730d722050435c8d1528
GitOutATown <aqumegeemail@gmail.com>
2015-08-27 00:15:38 -0700

    backup

--- src/main/scala/courses/bigdata_spark/week3/TextAnalysisEntityRes5.scala
+++ src/main/scala/courses/bigdata_spark/week3/TextAnalysisEntityRes5.scala
@@ -5,7 +5,7 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 import scala.util.matching.Regex
 
-object TextAnalysisEntityRes4 {
+object TextAnalysisEntityRes5 {
     
     def main(args: Array[String]): Unit = {
         
@@ -26,28 +26,25 @@ object TextAnalysisEntityRes4 {
                 
         // ------ Parsing ------------- //
         
-        trait RecordContent
-        case class ParsedRecord(val id: String, product: (String, String, String)) extends RecordContent
-        case class FailedRecord(val line: String) extends RecordContent
-        abstract class Record(val record: RecordContent, val flag: Int)
-        case class Parsed(override val record: ParsedRecord, override val flag: Int) extends Record(record, flag)
-        case class Failed(override val record: FailedRecord, override val flag: Int) extends Record(record, flag)
-        
         val dataFilePattern = """^(.+),"(.+)",(.*),(.*),(.*)""".r
         
         def removeQuotes(s: String): String = {
             s.filter(ch => ch != '\"')
         }
         
-        def parseDataFileLine(datafileLine: String, pattern: Regex): Record = {
+        type FailedRecord = (String, Int)
+        type ParsedRecord = (String, (String, String, String))
+        
+        def parseDataFileLine(datafileLine: String, pattern: Regex)
+            :Either[FailedRecord, ParsedRecord] = {
             pattern.findFirstIn(datafileLine) match {
-                case None => Failed(FailedRecord(datafileLine), -1)
+                case None => Left(datafileLine, -1)
                 case Some(success) =>
                     val pattern(grp1, grp2, grp3, grp4, _) = datafileLine
-                    if (grp1 == "\"id\"") Failed(FailedRecord(datafileLine), 0)
+                    if (grp1 == "\"id\"") Left(datafileLine, 0)
                     else {
                         val product = (grp2, grp3, grp4)
-                        Parsed(ParsedRecord(removeQuotes(grp1), product), 1)
+                        Right(removeQuotes(grp1), product)
                     }
                }
         }
@@ -56,13 +53,20 @@ object TextAnalysisEntityRes4 {
             sc.textFile(filePath).map(line => parseDataFileLine(line, dataFilePattern))
         }
         
-        def loadData(filePath: String): RDD[RecordContent] = {
+        def loadData(filePath: String) = {
             val raw = parseData(filePath)
             
-            val failed = raw.filter(record => record.flag == -1).map(record => record.record)
-            failed.take(10).foreach(line => println("Invalid datafile line: " + (filePath, line)))
+            val failed = raw.filter(record => record match{
+                case Left(record) => true
+                case Right(record) => false
+            })
+            failed.take(10).foreach(line => 
+                println("Invalid datafile line: " + (filePath, line)))
             
-            val valid = raw.filter(record => record.flag == 1).map(record => record.record).cache
+            val valid = raw.filter(record => record match{
+                case Right(record) => true
+                case Left(record) => false
+            }).cache
             
             val rawCount = raw.count()
             val failedCount = failed.count()
@@ -78,7 +82,9 @@ object TextAnalysisEntityRes4 {
         val amazonData = loadData(resource_path + AMAZON)
         
         // Examine
-        googleSmallData.take(3) foreach{line => println(line) }
+        googleSmallData.take(3) foreach{
+            line => println(line.right.get._1 + ": " + line.right.get._2) 
+        }
         
     }
     
